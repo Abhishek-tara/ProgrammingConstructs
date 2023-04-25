1d0e748e7eb2da17ed159b12260a105c4c0638f0
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-04-25 12:58:45 +0530

    Added Programming constructs problems

---
+++ ProgrammingConstructs/ProgrammingConstructs/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ProgrammingConstruct
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {   
+            ProgrammingConstruct programmingconstruct = new ProgrammingConstruct();
+            //programmingConstruct.AcceptInteger();
+            //programmingconstruct.ArithmeticProblem();
+            //programmingconstruct.TableOfTwo();
+            //programmingconstruct.SumOfSquare();
+            //programmingconstruct.Factorial();
+            //programmingconstruct.FlipCoin();
+            //programmingconstruct.ReverseString();
+            programmingconstruct.NaturalNumber();  
+        }
+    }
+}
