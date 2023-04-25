62e071fc1a079e5bb41a14ba2a65a6aa25dc7b29
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-04-25 13:36:00 +0530

    Added: Method to Find Largest of three

--- ProgrammingConstructs/ProgrammingConstructs/Program.cs
+++ ProgrammingConstructs/ProgrammingConstructs/Program.cs
@@ -5,14 +5,7 @@
         public static void Main(string[] args) 
         {   
             ProgrammingConstruct programmingconstruct = new ProgrammingConstruct();
-            //programmingConstruct.AcceptInteger();
-            //programmingconstruct.ArithmeticProblem();
-            //programmingconstruct.TableOfTwo();
-            //programmingconstruct.SumOfSquare();
-            //programmingconstruct.Factorial();
-            //programmingconstruct.FlipCoin();
-            //programmingconstruct.ReverseString();
-            programmingconstruct.NaturalNumber();  
+            programmingconstruct.LargestOfThree();  
         }
     }
 }
